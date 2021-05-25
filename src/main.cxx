80809c1829a477dffe779e3946cf3d66a55c6ca6
Andrew Potter <agpotter@gmail.com>
2021-05-24 23:37:01 -0700

    Cleanup, be cuter on final day

--- src/main.cxx
+++ src/main.cxx
@@ -5,59 +5,84 @@
 #include "date/date.h"
 #include "date/tz.h"
 
-using namespace date::literals;
-using namespace std::chrono_literals;
+namespace {
+    using namespace date::literals;
+    using namespace std::chrono_literals;
+
+    constexpr auto expo_day = date::local_days{6_d/date::June/2021} + 10h;
+    const auto expo_time = date::make_zoned("Asia/Tokyo", expo_day).get_sys_time();
+}
 
 class CountdownWindow : public Gtk::Window
 {
 public:
     CountdownWindow();
 
-    Gtk::Grid *grid;
+    Gtk::Grid *m_grid;
     Gtk::Label *m_days;
     Gtk::Label *m_hours;
-    Gtk::Label *colon_hm;
-    Gtk::Label *colon_ms;
 
-    sigc::connection timeout;
-    const date::zoned_time<std::chrono::duration<long>> expoday_jp;
-    const std::chrono::time_point<date::local_t, std::chrono::duration<long>> expoday;
+    sigc::connection m_timeout;
 
-    void update();
+    bool update() const;
 };
 
-void CountdownWindow::update()
+bool CountdownWindow::update() const
 {
-    auto now = date::make_zoned(date::current_zone(), std::chrono::system_clock::now()).get_local_time();;
-    auto diff = expoday - now;
+    auto now = std::chrono::system_clock::now();
+    auto diff = expo_time - now;
     auto secs = std::chrono::duration_cast<std::chrono::seconds>(diff).count();
     auto mins = secs / 60;
     auto hours = mins / 60;
     auto days = hours / 24;
-    secs = secs % 60;
-    mins = mins % 60;
-    hours = hours % 24;
-    {
+
+    if (days > 0) {
         std::stringstream ss;
-        ss << days << " days";
+        ss << days << " day";
+        if (days > 1) {
+            ss << 's';
+        }
         m_days->set_label(ss.str());
+    } else {
+        if (secs > 0) {
+            m_days->set_line_wrap(true);
+            m_days->set_line_wrap_mode(Pango::WRAP_WORD);
+            m_days->set_label("Less than a day!");
+        } else {
+            m_days->set_label("Miku Miku Miku Miku Miku Miku Miku Miku Miku Miku");
+            m_days->set_line_wrap(true);
+            m_days->set_lines(5);
+            m_grid->set_valign(Gtk::ALIGN_START);
+            m_grid->set_halign(Gtk::ALIGN_START);
+            m_grid->remove_row(1);
+            return false;
+        }
     }
 
-    {
-        std::stringstream ss;
-        ss << std::setw(2) << std::setfill('0') << hours << ":";
-        ss << std::setw(2) << std::setfill('0') << mins << ":";
-        ss << std::setw(2) << std::setfill('0') << secs;
+    std::stringstream ss;
+    if (hours > 0) {
+        ss << std::setw(1) << std::setfill('0') << hours % 24 << ":";
+    }
+    if (mins > 0) {
+        ss << std::setw(2) << std::setfill('0') << mins % 60 << ":";
+    }
+    if (secs > 0) {
+        if (secs > 10) {
+            ss << std::setw(2);
+        } else {
+            ss << std::setw(1);
+        }
+        ss << std::setfill('0') << secs % 60;
         m_hours->set_label(ss.str());
     }
+
+    return true;
 }
 
 CountdownWindow::CountdownWindow() :
-    grid(Gtk::manage(new Gtk::Grid())),
+    m_grid(Gtk::manage(new Gtk::Grid())),
     m_days(Gtk::manage(new Gtk::Label())),
-    m_hours(Gtk::manage(new Gtk::Label())),
-    expoday_jp(date::make_zoned("Asia/Tokyo", date::local_days{6_d/6/2021} + 10h)),
-    expoday(date::make_zoned(date::current_zone(), expoday_jp).get_local_time())
+    m_hours(Gtk::manage(new Gtk::Label()))
 {
     set_title("Miku Expo Online Countdown");
     set_default_size(400, 500);
@@ -65,17 +90,17 @@ CountdownWindow::CountdownWindow() :
     m_days->set_name("days");
     m_hours->set_name("hours");
 
-    grid->attach(*m_days, 0, 0);
-    grid->attach(*m_hours, 0, 1);
-    grid->set_halign(Gtk::ALIGN_CENTER);
-    grid->set_valign(Gtk::ALIGN_CENTER);
-    add(*grid);
+    m_grid->attach(*m_days, 0, 0);
+    m_grid->attach(*m_hours, 0, 1);
+    m_grid->set_halign(Gtk::ALIGN_CENTER);
+    m_grid->set_valign(Gtk::ALIGN_CENTER);
+    add(*m_grid);
+
+    m_grid->show_all();
 
     update();
 
-    grid->show_all();
-
-    timeout = Glib::signal_timeout().connect_seconds([this] { update(); return true; }, 1);
+    m_timeout = Glib::signal_timeout().connect_seconds([this] { return update(); }, 1);
 }
 
 int main (int argc, char *argv[])
