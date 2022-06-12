fc9856f500829c247d1ef316815a57391dba6656
Andrew Potter <agpotter@gmail.com>
2022-06-12 14:21:47 -0700

    Fix make_window_and_run

--- src/main.cxx
+++ src/main.cxx
@@ -184,8 +184,9 @@ bool CountdownGrid::update()
 class CountdownWindow : public Gtk::ApplicationWindow
 {
 public:
-    CountdownWindow() :
+    CountdownWindow(const Glib::RefPtr<Gtk::Application>& app) :
         Glib::ObjectBase("CountdownWindow"),
+        Gtk::ApplicationWindow(app),
         m_box(Gtk::manage(new Gtk::Box(Gtk::Orientation::VERTICAL))),
         m_stack(Gtk::manage(new Gtk::Stack())),
         m_stack_switcher(Gtk::manage(new Gtk::StackSwitcher()))
@@ -405,5 +406,36 @@ private:
 int main (int argc, char *argv[])
 {
     auto app = MikuApplication::create("dance._39music.MikuExpoCountdown");
-    return app->make_window_and_run<CountdownWindow>(argc, argv);
+
+    if constexpr (!GTKMM_CHECK_VERSION(4,8,0)) {
+        Gtk::Window *m_run_window = nullptr;
+        app->signal_activate().connect([&app, &m_run_window] () {
+            if (!m_run_window) {
+                auto window = new CountdownWindow(app);
+                bool was_floating = g_object_is_floating(window->gobj());
+                window->Gtk::Object::set_manage();
+                if (!was_floating && g_object_is_floating(window->gobj()))
+                    g_object_ref_sink(window->gobj());
+                m_run_window = window;
+                app->add_window(*window);
+                window->set_application(app);
+                window->present();
+            } else {
+                m_run_window->present();
+            }
+        });
+
+        app->signal_window_removed().connect([&m_run_window] (Gtk::Window* window) {
+            if (window == m_run_window)
+            {
+                if (window && window->gobj())
+                    gtk_window_destroy(window->gobj());
+                m_run_window = nullptr;
+            }
+        });
+
+        app->run(argc, argv);
+    } else {
+        return app->make_window_and_run<CountdownWindow>(argc, argv, app);
+    }
 }
